092ef0a7b62d5cd352bc630fc6828d7d04c3d0c5
Daniel du Preez <dandupreez@legiondev.co.za>
2024-01-04 15:46:47 +0200

    Add URL endpoint for yearly and all data

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template('home.html', data=stations.to_html())
 
 
-@app.route('/api/v1/<station>/<date>/')
+@app.route('/api/v1/<station>/<date>')
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
@@ -20,6 +20,23 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == '__main__':
